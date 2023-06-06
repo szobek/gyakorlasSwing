6bb60206522886d966c38dd951f7837f76ccc7ab
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-06 20:19:10 +0200

    fix bug

--- src/orabolt/db.sql
+++ src/orabolt/db.sql
@@ -4,7 +4,7 @@ use oraBolt;
 
 create table orak (
 ID int auto_increment unique ,
-name vachar(50) not null,
+name varchar(50) not null,
 tipus varchar(10) not null,
 ar int,
 vizallo BOOLEAN default false
