30a7677acce2e7c338bf0a7d19a4224ee7c4bfba
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-06 17:21:33 +0200

    POJO and enum file success

---
+++ src/orabolt/OraTipusok.java
@@ -0,0 +1,25 @@
+package orabolt;
+
+public enum OraTipusok {
+	KARORA("karóra"), FALIORA("falióra"), EBRESZTOORA("ébresztõóra"), STOPPERORA("stopperóra");
+
+	private String type;
+
+	private OraTipusok(String type) {
+		this.type=type;
+	}
+
+	public static OraTipusok convertToEnum(String tipus) {
+		for (OraTipusok elem : OraTipusok.values()) {
+			if (elem.type.equals(tipus)) {
+				return elem;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return type;
+	}
+}
