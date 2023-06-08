1509847664f685b8748278f2dcc62cda6c12d7ef
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-08 16:13:00 +0200

    add insert

--- src/orabolt/db.sql
+++ src/orabolt/db.sql
@@ -9,3 +9,8 @@ tipus varchar(10) not null,
 ar int,
 vizallo BOOLEAN default false
 );
+
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Festina', 'karóra', '110000', '1');
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Casio', 'karóra', '34000', '1');
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Rolex', 'karóra', '1000000', '1');
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Hama', 'ébresztõóra', '4400', '0');
