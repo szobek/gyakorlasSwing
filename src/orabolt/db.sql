ab4b2e6a2131663c0676a2ac59baf2bb13ff9ed9
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-10 23:49:45 +0200

    change sql file - type lentgh

--- src/orabolt/db.sql
+++ src/orabolt/db.sql
@@ -5,7 +5,7 @@ use oraBolt;
 create table orak (
 ID int auto_increment unique ,
 name varchar(50) not null,
-tipus varchar(10) not null,
+tipus varchar(20) not null,
 ar int,
 vizallo BOOLEAN default false
 );
