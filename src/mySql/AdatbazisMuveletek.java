8e4dc59b24f18ffed0acb643ed6631cf430475cf
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-06-12 22:19:13 +0200

    DBHandle class hozzáadása

--- src/mySql/AdatbazisMuveletek.java
+++ src/mySql/AdatbazisMuveletek.java
@@ -1,12 +1,13 @@
 package mySql;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Alap szerkezet az Adatbázis müveletekhez.
  * implementálva egyéni Adatbázis kezelõt lehet létrehozni.
- * @param <T> megadásával példányosítható más osztályokkal.
+ * @param 		<T> megadásával példányosítható más osztályokkal.
  */
 public interface AdatbazisMuveletek<T> {
 
@@ -24,21 +25,40 @@ public interface AdatbazisMuveletek<T> {
 	 * 
 	 * @throws SQLException
 	 */
-	public void sqlKapcsolat() 
+	public Connection sqlKapcsolat() 
 	throws SQLException;
 	
 	public void sqlKapcsolatBontas()
 	throws SQLException;
 	
+	/**
+	 * CRUD egyik müvelete a Készítés megvalósítása.
+	 * @param 		generic példányosított Osztály változó megadása paraméterkén.
+	 * @throws 		SQLException
+	 */
 	public void sqlKeszites(T generic)
 	throws SQLException;
 	
+	/**
+	 * CRUD egyik müvelete a Beolvasas megvalósítása.
+	 * @return 		List példányosított adatszerkezetben visszaadja azokat az elemeket amik,
+	 * 				példányosítva lettek mint genricus Osztály.
+	 * @throws 		SQLException
+	 */
 	public List<T> sqlBeolvasas()
 	throws SQLException;
 	
+	/**
+	 * CRUD egyik müvelete a Módosítás megvalósítása.
+	 * @throws SQLException
+	 */
 	public void sqlModositas()
 	throws SQLException;
 	
+	/**
+	 * CRUD egyik müvelete a Törlés megvalósítása.
+	 * @throws SQLException
+	 */
 	public void sqlTorles()
 	throws SQLException;
 }
