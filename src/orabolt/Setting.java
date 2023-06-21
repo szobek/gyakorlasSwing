ce0598e8c8fb788ffc91d4f6bc8dd7a21c1c9a06
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-21 20:20:41 +0200

    convert project to maven

--- src/orabolt/Setting.java
+++ src/orabolt/Setting.java
@@ -8,25 +8,25 @@ import java.util.Map;
 import java.util.Properties;
 
 public class Setting {
-	private static Map<String, String> beallitasok = new HashMap<String, String>();
-	
+	private static Map<String, String> beallitasok = new HashMap<>();
+
 	public static void putDData() {
 
 		String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
 		String appConfigPath = rootPath + "appSettings.prop";
-		
+
 		Properties appProps = new Properties();
 		try {
 			appProps.load(new FileInputStream(appConfigPath));
-			
+
 
 			beallitasok.put("DbUrl", appProps.getProperty("DbUrl"));
 			beallitasok.put("DbPort",appProps.getProperty("DbPort"));
 			beallitasok.put("DbName",appProps.getProperty("DbName"));
 			beallitasok.put("DbUser",appProps.getProperty("DbUser"));
 			beallitasok.put("DbPsw",appProps.getProperty("DbPsw"));
-			
-			
+
+
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -40,9 +40,9 @@ public class Setting {
 	public Map<String, String> getBeallitasok() {
 		return beallitasok;
 	}
-	
-	
-	
-	
+
+
+
+
 
 }
